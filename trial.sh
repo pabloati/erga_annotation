5183d0fe8ca285811454e39583e93a20c789dd0d
pabloati <pabloatienzalo@gmail.com>
2026-05-05 16:06:30 +0200

    Initial commit

---
+++ trial.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --qos long
+#SBATCH --mem 4G
+#SBATCH -c 2
+#SBATCH -t 10-00:00:00
+#SBATCH -o logs/canis.out
+#SBATCH -e logs/canis.out
+#SBATCH -J jackal_annot
+
+module load anaconda
+source activate snakemake
+
+#snakemake --use-conda --jobs 10 --snakefile ../snakefile --configfile test_config.yaml --rerun-incomplete --executor slurm --default-resources slurm_account=gge
+
+/home/patienza/oscars/LR_annotation/sqanti_evidence -c configs/trial.yaml -j 1 -s -se "--default-resources slurm_account=gge" -R -e "--rerun-triggers input params code --max-jobs-per-second 5 --max-status-checks-per-second 5"
